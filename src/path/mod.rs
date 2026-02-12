867bcaf85b6cec6a3a5fe50f955ef7346e7f9a99
Eira <skuldnorniern@gmail.com>
2026-02-12 19:55:25 +0900

    feat: prepare for glob

--- src/path/mod.rs
+++ src/path/mod.rs
@@ -1,6 +1,8 @@
 mod expander;
+mod glob;
 
 pub use expander::PathExpander;
+pub use glob::expand_glob;
 
 pub fn home_dir() -> Option<std::path::PathBuf> {
     std::env::var_os("HOME").map(std::path::PathBuf::from)
